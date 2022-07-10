6569a095773e9cf97c6b408c0dbe932533bd5c95
Oscar <oscar@superiorcsgroup.com>
2022-07-11 01:47:40 +0530

    adding style for create post

--- components/post/createPost.tsx
+++ components/post/createPost.tsx
@@ -4,6 +4,29 @@ import styles from "./styles.module.css";
 import { VscDeviceCameraVideo } from "react-icons/vsc";
 import { MdOutlineInsertPhoto } from "react-icons/md";
 import { RiUserSmileLine } from "react-icons/ri";
+interface PostActionInterface {
+  key: string;
+  title: string;
+  icon: React.ReactNode | React.ReactNode[];
+}
+const postActions: PostActionInterface[] = [
+  {
+    key: "liveVideo",
+    title: "Live Video",
+    icon: <VscDeviceCameraVideo />,
+  },
+  {
+    key: "photoVideo",
+    title: "Photo/Video",
+    icon: <MdOutlineInsertPhoto />,
+  },
+  {
+    key: "feeling",
+    title: "Feeling",
+    icon: <RiUserSmileLine />,
+  },
+];
+
 export default function CreatePost() {
   return (
     <div className={styles.createPost}>
@@ -19,24 +42,12 @@ export default function CreatePost() {
         <input placeholder="What's happening?" className={styles.textarea} />
       </div>
       <div className="d-flex justify-content-between align-items-center">
-        <div>
-          <span>
-            <VscDeviceCameraVideo />
-          </span>
-          Live Video
-        </div>
-        <div>
-          <span>
-            <MdOutlineInsertPhoto />
-          </span>
-          Photo/Video
-        </div>
-        <div>
-          <span>
-            <RiUserSmileLine />
-          </span>
-          Feeling
-        </div>
+        {postActions.map((action) => (
+          <div className={styles.postAction} key={action.key}>
+            <span>{action.icon}</span>
+            <span>{action.title}</span>
+          </div>
+        ))}
         <button type="submit" className="btn btn-primary rounded-5">
           Post
         </button>
