3d95830be3cd784713b40258b3b52080beea2639
surendra1990 <surendra@fourtek.com>
2022-08-18 15:57:51 +0530

    adding more files

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -2,24 +2,46 @@ import InputGroup from "@/globalComponents/inputGroup/input-group.component";
 import { PROJECT_TITLE } from "@/utils/constant";
 import { ChangeEvent, useState } from "react";
 import { AiFillFacebook, AiOutlineSearch } from "react-icons/ai";
+import ICONS from "@/globalComponents/icons";
 import styles from "./navbar.module.css";
+import { useDispatch } from "react-redux";
+import { updateIsOpenSidebar } from "@/slice/generalSlices";
+import { useSelector } from "store/store";
+
+const { ListSidebar } = ICONS;
 
 interface NavbarProps {
   isLoggedIn: boolean;
 }
 export default function Navbar({ isLoggedIn }: NavbarProps) {
+  const dispatch = useDispatch();
+  const { isOpenSidebar } = useSelector((state) => state.general);
   const [search, setSearch] = useState("");
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
   const handleSearchChange = (e: ChangeEvent<any>) => {
     setSearch(e.target.value);
   };
+
+  const handleToggleNavbar = () => {
+    dispatch(updateIsOpenSidebar(!isOpenSidebar));
+  };
+
   return (
     <div className={styles.navbar}>
       <div className={styles.navbar_logo}>
-        <span>
-          <AiFillFacebook />
-        </span>
+        <span>{/* <AiFillFacebook /> */}</span>
         <span>{PROJECT_TITLE}</span>
       </div>
+      <a
+        className={`${styles.md_list_icon} d-lg-none d-md-none d-block`}
+        onClick={handleToggleNavbar}
+      >
+        <ListSidebar onClick={toggle} />
+      </a>
+
       {isLoggedIn && (
         <div className={styles.navbar_search}>
           <InputGroup
